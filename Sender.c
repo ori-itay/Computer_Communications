f0099dfed938fda25648dddb514ac7fd3da4f834
Ori <origlassman@mail.tau.ac.il>
2019-03-08 12:24:52 +0200

    test2

--- Sender.c
+++ Sender.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 
 int main(int argc, char** argv) {
+	if (argc != 4) {
+		fprintf(stderr, "Error: not enough arguments were provided\n");
+		exit(1);
+	}
+	char* ip = argv[1]; char* port = argv[2]; char* file_name = argv[3];
 
 
 
