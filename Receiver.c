3982c4ccd66f01d04cbe77678407f0facb6e2fce
Origithub <39296766+Origithub@users.noreply.github.com>
2019-03-08 17:00:13 +0200

    Add files via upload

---
+++ Receiver.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "winsock2.h"
+
+void Init_Winsock();
+
+int main(int argc, char** argv) {
+	if (argc != 3) {
+		printf("Error: not enough arguments were provided!\n");
+		exit(1);
+	}
+	Init_Winsock();
+
+
+
+
+
+
+	return 0;
+}
+
+void Init_Winsock() {
+	WSADATA wsaData;
+	int iResult = WSAStartup(MAKEWORD(2, 2), &wsaData);
+	if (iResult != NO_ERROR)
+		printf("Error at WSAStartup()\n");
+	exit(1);
+}
