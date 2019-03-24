956dd59125a9edbeddf3ea1d602e10ae4d2afbf5
Ori <origlassman@mail.tau.ac.il>
2019-03-24 23:04:32 +0200

    implemented listening socket on receiver

--- Receiver.c
+++ Receiver.c
@@ -5,13 +5,41 @@
 void Init_Winsock();
 
 int main(int argc, char** argv) {
+	int listenfd = -1;
+	int connfd = -1;
+	int sockAddrInLength = sizeof(struct sockaddr_in);
+	struct sockaddr_in receiver_addr, channel;
 	if (argc != 3) {
 		printf("Error: not enough arguments were provided!\n");
 		exit(1);
 	}
 	Init_Winsock();
 
+	if ((listenfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
+		fprintf(stderr, "%s\n", strerror(errno));
+		exit(1);
+	}
 
+	unsigned int port = (unsigned int)strtoul(argv[1], NULL, 10); //get channel's port number
+	memset(&receiver_addr, 0, sizeof(receiver_addr));
+	receiver_addr.sin_family = AF_INET;
+	receiver_addr.sin_port = htons(port);
+	receiver_addr.sin_addr.s_addr = inet_addr(INADDR_ANY);
+
+	if (0 != bind(listenfd, (struct sockaddr*) &receiver_addr, sizeof(receiver_addr))){
+		fprintf(stderr, "%s\n", strerror(errno));
+		return 1;
+	}
+	if (0 != listen(listenfd, 10)){
+		fprintf(stderr, "%s\n", strerror(errno));
+		return 1;
+	}
+	connfd = accept(listenfd, (struct sockaddr*) &channel, &sockAddrInLength);
+	if (connfd < 0)
+	{
+		fprintf(stderr, "%s\n", strerror(errno));
+		socketclose(connfd);
+	}
 
 
 
